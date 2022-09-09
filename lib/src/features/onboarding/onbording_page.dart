057824ba52b481ead10d30d3491f7c2b8e5d5cc8
Masahiro Aoki <rafe.kun@gmail.com>
2022-09-09 20:05:04 +0900

    add ci

---
+++ lib/src/features/onboarding/onbording_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class OnboardingPage extends ConsumerWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
